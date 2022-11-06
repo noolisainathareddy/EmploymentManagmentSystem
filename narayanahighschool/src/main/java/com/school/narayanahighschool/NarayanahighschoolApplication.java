dc10275de98eef39cb349551ade8542c42001896
noolisainathareddy <nooli.sainathareddy@gmail.com>
2022-11-06 17:42:52 -0600

    ReactProject_firstCommit

---
+++ narayanahighschool/src/main/java/com/school/narayanahighschool/NarayanahighschoolApplication.java
@@ -0,0 +1,13 @@
+package com.school.narayanahighschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarayanahighschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarayanahighschoolApplication.class, args);
+	}
+
+}

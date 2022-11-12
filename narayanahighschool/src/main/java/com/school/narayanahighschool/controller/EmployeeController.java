11c7f230aafe8985e6e67c55559aa8f916c95174
noolisainathareddy <nooli.sainathareddy@gmail.com>
2022-11-12 07:57:22 -0600

    new controller for add employees to the list

--- narayanahighschool/src/main/java/com/school/narayanahighschool/controller/EmployeeController.java
+++ narayanahighschool/src/main/java/com/school/narayanahighschool/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.school.narayanahighschool.controller;
 
+import com.school.narayanahighschool.model.Employee;
 import com.school.narayanahighschool.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -12,7 +15,17 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    private Employee employee;
+
     //GetAllEmployees
-    public List<Employees> getAllEmployees(){
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+
+    @PostMapping("/add")
+    public Employee addEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
     }
 }
